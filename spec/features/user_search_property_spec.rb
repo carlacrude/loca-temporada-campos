3d98f76d29134c408332864948658cbc389cb255
Carla Carla <carlacristinecrudedossanto@correio.itau.com.br>
2018-07-12 14:26:48 -0300

    [WIP] - Buscar imóveis por região

--- spec/features/user_search_property_spec.rb
+++ spec/features/user_search_property_spec.rb
@@ -1,12 +1,14 @@
-require 'rails helper'
+require 'rails_helper'
 
 feature 'search property by region' do
   scenario 'successfully' do
     amazonas = Region.create(name: 'Amazonas')
-    apartamento = PropertyType.create(name: 'Apartamento')
-    property = Property.create(title:'Apartamento para aluguel temporada', 
+    campos = Region.create(name: 'Campos')
+    property_type_apartamento= PropertyType.create(name: 'Apartamento')
+    property_type_casa = PropertyType.create(name: 'Casa')
+    apartamento = Property.create(title:'Apartamento para aluguel temporada', 
                                 description: 'Lindo apartamento com vista para praia e sacada', 
-                                property_type: apartamento,
+                                property_type: property_type_apartamento,
                                 region: amazonas, 
                                 rent_purpose: 'férias', 
                                 area: 10,
@@ -19,12 +21,9 @@ feature 'search property by region' do
                                 maximum_rent: 20,
                                 daily_rate: 500,
                                 photo: File.new(Rails.root.join('spec', 'support','teste.jpg')))
-
-    campos = Region.create(name: 'Campos')
-    casa = PropertyType.create(name: 'Casa')
-    property = Property.create(title:'Casa para aluguel temporada', 
+    casa = Property.create(title:'Casa para aluguel temporada', 
                                 description: 'Linda casa com vista para praia e sacada', 
-                                property_type: casa,
+                                property_type: property_type_casa,
                                 region: campos, 
                                 rent_purpose: 'férias', 
                                 area: 10,
@@ -37,10 +36,17 @@ feature 'search property by region' do
                                 maximum_rent: 20,
                                 daily_rate: 500,
                                 photo: File.new(Rails.root.join('spec', 'support','teste.jpg')))
-   
+                                
     visit root_path
-    click_on 'Buscar imóvel'
+    click_on 'Buscar um imóvel'
     fill_in 'Região', with: 'Amazonas'
+  
+    expect(page).to have_css("img[src*='teste.jpg']")
+    expect(page).to have_css('p', text: amazonas.name)
+    expect(page).to have_css('p', text: apartamento.title)
+    expect(page).to have_css('p', text: apartamento.maximum_guests)
+    expect(page).to have_css('h1', text: apartamento.property_type.name)
 
+    expect(page).to_not have_css('p', text: campos.name)
   end
 end
