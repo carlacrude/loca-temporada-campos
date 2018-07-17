c17b79400786c9cc5e0bc7a4323f2ff3f9d14cea
Carla Carla <carlacristinecrudedossanto@correio.itau.com.br>
2018-07-17 11:25:47 -0300

    Login usuário

--- spec/features/user/user_login_spec.rb
+++ spec/features/user/user_login_spec.rb
@@ -13,4 +13,17 @@ feature 'user login' do
         expect(page).to have_content('Signed in successfully.')
         expect(page).not_to have_content('Login')
     end
+
+    scenario 'do not login with invalid e-mail' do
+        user = User.create(email: 'user@user.com', password:'123456')
+
+        visit root_path
+        click_on 'Login usuário'
+        fill_in 'Email', with: 'user2@user.com'
+        fill_in 'Senha', with: user.password
+        click_on 'Entrar'
+
+        expect(page).to have_content('Invalid Email or password.')
+        expect(page).to have_content('Login')
+    end
 end
