4cf73c9e19da96a611873eab466f6e234f1eb8ab
Carla Carla <carlacristinecrudedossanto@correio.itau.com.br>
2018-07-17 12:09:06 -0300

    log out - 2 cenários

--- spec/features/user/user_login_spec.rb
+++ spec/features/user/user_login_spec.rb
@@ -12,6 +12,7 @@ feature 'user login' do
 
         expect(page).to have_content('Signed in successfully.')
         expect(page).not_to have_content('Login')
+        expect(page).to have_content('Sair')
     end
 
     scenario 'do not login with invalid e-mail' do
@@ -27,3 +28,32 @@ feature 'user login' do
         expect(page).to have_content('Login')
     end
 end
+
+feature 'logout' do
+    scenario 'successfully' do
+        user = User.create(email: 'user@user.com', password:'123456')
+
+        visit root_path
+        click_on 'Login usuário'
+        fill_in 'Email', with: user.email
+        fill_in 'Senha', with: user.password
+        click_on 'Entrar'
+        click_on 'Sair'
+
+        expect(page).to have_content('Login usuário')
+        expect(page).not_to have_content(user.email)
+    end  
+    
+    scenario 'do not show sair button without previouslly login' do
+        user = User.create(email: 'user@user.com', password:'123456')
+
+        visit root_path
+        click_on 'Login usuário'
+        fill_in 'Email', with: user.email
+        fill_in 'Senha', with: user.password
+
+        expect(page).to have_content('Log in')
+        expect(page).not_to have_content('Sair')
+    end 
+end
+
